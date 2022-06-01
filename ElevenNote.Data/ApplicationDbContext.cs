87b08d5a6ee45a2a418cccaa704778a63c078db0
Tracy Redman <tracyredman@Tracys-MacBook-Air-2.local>
2022-05-31 20:11:22 -0400

    completed App DbContext Setup

---
+++ ElevenNote.Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options):base(options)
+        {}
+    }
